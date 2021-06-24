4fce956343e67e1f2a5227cf4708fe649e006292
Junior Alves <junioralves.jav@gmail.com>
2021-06-23 22:49:15 -0300

    Pages folder has been created

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss'
+
+export function NewRoom() {
+    const { user } = useAuth()
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Nome da sala"
+                        />
+                        <Button type="submit">
+                            Criar uma sala
+                        </Button>
+                    </form>
+
+                    <p>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
