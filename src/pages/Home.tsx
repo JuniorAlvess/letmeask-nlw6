87bbf55e9b3cb8796c0def0479878519a4b70cd1
Junior Alves <junioralves.jav@gmail.com>
2021-06-26 01:13:12 -0300

    Home component completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,6 +39,11 @@ export function Home() {
             return;
         }
 
+        if (roomRef.exists()) {
+            alert('Room already closed.');
+            return;
+        }
+
         history.push(`/rooms/${roomCode}`)
     }
 
