eae2058a6d291915e30b980899f3300b533f08e8
Junior Alves <junioralves.jav@gmail.com>
2021-07-09 23:59:18 -0300

    handleJoinRoom function has been updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,7 @@ export function Home() {
             return;
         }
 
-        if (roomRef.exists()) {
+        if (roomRef.val().endedAt) {
             alert('Room already closed.');
             return;
         }
